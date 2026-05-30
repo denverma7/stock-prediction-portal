d2b2d5e63869c74aaa1cecc26a1edca5c1ac150b
denverma7 <denverma7@gmail.com>
2026-05-30 15:24:03 +0500

    registration page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,16 +1,25 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './assets/css/style.css'
-import Footer from './components/Footer'
-import Header from './components/Header'
 import Main from './components/Main'
+import Register from './components/Register'
+import Login from './components/Login'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 function App() {
 
   return (
     <>
+      <Router>
       <Header />
-      <Main />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
       <Footer />
+      </Router>
     </>
   )
 }
